8f3b9f874f94459cd1102e577989b14b1631ff5f
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-08-18 10:23:29 +0100

    chore: Add about endpoint

--- client/modules/router/main.js
+++ client/modules/router/main.js
@@ -182,6 +182,13 @@ Router.initPackageRoutes = () => {
       }
     });
 
+    shop.route("/about", {
+      name: "about",
+      action() {
+        ReactionLayout({ template: "aboutRC" });
+      }
+    });
+
     // get package registry route configurations
     for (const pkg of pkgs) {
       const newRoutes = [];
