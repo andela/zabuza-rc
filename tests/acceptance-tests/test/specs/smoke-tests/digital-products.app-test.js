16882bd9c4efe5244c47ad549a3f1e9c6a877cac
DanielAmah <amahdanieljack@gmail.com>
2017-08-31 09:32:51 +0100

    buy and download digital product

--- tests/acceptance-tests/test/specs/smoke-tests/digital-products.app-test.js
+++ tests/acceptance-tests/test/specs/smoke-tests/digital-products.app-test.js
@@ -11,8 +11,8 @@ beforeEach(function () {
   browser.url(baseUrl);
 });
 
-describe("Paystack payment", function () {
-  it("Should checkout product with paystack", function () {
+describe("Digital Products", function () {
+  it("Should be available to shop and download", function () {
     const eleMap = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-map.yml", "utf8"));
     const eleIds = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-ids.yml", "utf8"));
     const usrData = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/config/user-data.yml", "utf8"));
@@ -20,23 +20,24 @@ describe("Paystack payment", function () {
     helper.startUp(eleMap, eleIds, getId, usrData, browser);
 
     browser.click(".brand");
-    browser.pause("6000");
+    browser.waitForExist("#Y3scxJ2CCQ6xxsT9k");
     browser.click("#Y3scxJ2CCQ6xxsT9k");
     browser.pause("6000");
     browser.scroll(0, 300);
-    browser.pause("5000");
+    browser.waitForExist(".js-add-to-cart");
     browser.click(".js-add-to-cart");
-    browser.pause("2000");
+    browser.waitForExist(".cart-alert-checkout");
     browser.click(".cart-alert-checkout");
     browser.pause("3000");
     browser.scroll(0, 500);
+    browser.pause("2000");
     browser.click(eleMap.free_shipping);
     browser.pause("4000");
     browser.click(eleMap.paystack_checkout);
     browser.pause("2000");
     browser.setValue("#payerName", "");
     browser.setValue("#payerName", "Daniel Amah");
-    browser.pause("1000");
+    browser.waitForExist("#completeOrder");
     browser.click("#completeOrder");
     browser.pause("10000");
     helper.paymentHelper(eleMap, eleIds, getId, browser);
