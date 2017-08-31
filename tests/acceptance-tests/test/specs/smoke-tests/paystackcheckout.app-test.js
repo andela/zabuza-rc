ee608593a4806d5f591b2a9c8b54d1b985af5ca8
seunadex <seun.adekunle@andela.com>
2017-08-31 12:23:55 +0100

    refactor

--- tests/acceptance-tests/test/specs/smoke-tests/paystackcheckout.app-test.js
+++ tests/acceptance-tests/test/specs/smoke-tests/paystackcheckout.app-test.js
@@ -11,7 +11,7 @@ beforeEach(function () {
   browser.url(baseUrl);
 });
 
-xdescribe("Paystack payment", function () {
+describe("Paystack payment", function () {
   it("Should checkout product with paystack", function () {
     const eleMap = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-map.yml", "utf8"));
     const eleIds = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-ids.yml", "utf8"));
